04dbfb5b1672fa0212c583a7097acdacde7a3b06
Deen Bashir <deen-bashir@hotmail.com>
2022-11-28 20:03:35 +0000

    expanded ui to show employee data from api

--- UI/crud.ui/src/app/app.module.ts
+++ UI/crud.ui/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,15 +7,9 @@ import { AppComponent } from './app.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmployeesListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EmployeesListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- UI/crud.ui/src/app/components/employees/employees-list/employees-list.component.ts
+++ UI/crud.ui/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -7,32 +8,16 @@ import { Employee } from 'src/app/models/employee.model';
   styleUrls: ['./employees-list.component.css'],
 })
 export class EmployeesListComponent implements OnInit {
-  employees: Employee[] = [
-    {
-      id: '123',
-      name: 'John Doe',
-      email: 'john.doe@hello.com',
-      phone: 31231232,
-      salary: 1000,
-      department: 'HR',
-    },
-    {
-      id: '456',
-      name: 'Jane Doe',
-      email: 'jane.doe@hello.com',
-      phone: 12312,
-      salary: 101241200,
-      department: 'Software',
-    },
-    {
-      id: '456',
-      name: 'Sam Smith',
-      email: 'sam.mith@hello.com',
-      phone: 5235235262,
-      salary: 1,
-      department: 'Marketing',
-    },
-  ];
-  constructor() {}
-  ngOnInit(): void {}
+  employees: Employee[] = [];
+  constructor(private employeesService: EmployeesService) {}
+  ngOnInit(): void {
+    this.employeesService.getAllEmployees().subscribe({
+      next: (employees) => {
+        this.employees = employees;
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
+  }
 }
