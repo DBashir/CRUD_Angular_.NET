602787e9e63069e9358755b8eb44f3c803510fad
Deen Bashir <deen-bashir@hotmail.com>
2022-11-28 19:34:42 +0000

    created backend api db

---
+++ API/Crud_API/Crud_API/Data/CrudAPIDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Crud_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_API.Data
+{
+    public class CrudAPIDBContext : DbContext
+    {
+        public CrudAPIDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ API/Crud_API/Crud_API/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud_API.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid guid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public long Salary { get; set; }
+        public string Department { get; set; }
+
+    }
+}
